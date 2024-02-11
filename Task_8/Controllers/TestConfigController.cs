d222579803f6d13f3ae8051527e73493767e68a8
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-11 18:50:25 +0500

    Test config

---
+++ Task_8/Controllers/TestConfigController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Task_8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestConfigController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get(IConfiguration config)
+        {
+            //IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            return Ok(config.GetSection("Settings:BlackList").Get<string[]>());
+        }
+    }
+}
